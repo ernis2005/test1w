3acd2157e3403aae7cdea50caf88c2199490e8d5
ernis2005 <nhtfxnhtfx.9@gmail.com>
2024-04-08 11:56:44 +0600

    add metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,12 @@ import ProviderRedux from "./store/Provider/Provider";
 
 import "./layouyt.scss";
 import Haeder from "../components/Haeder/Haeder";
+export const metadata = {
+  title: "Оптимизация производства",
+  description:
+    "Мы специализируемся на разработке инновационных методов оптимизации производства. Наша цель - улучшить эффективность и экономическую эффективность производственных процессов. Мы предлагаем интегрированные решения, основанные на передовых технологиях и лучших практиках отрасли.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
